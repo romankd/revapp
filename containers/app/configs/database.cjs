6074a9e7988b3b90b20112ac4a424521e272d2ae
rmk <roman.kurach1@gmail.com>
2024-06-15 22:35:05 +0200

    updated workflow configs

--- containers/app/configs/database.cjs
+++ containers/app/configs/database.cjs
@@ -6,7 +6,7 @@ const db_password = process.env.DB_PASSWORD;
 const db_name = process.env.DB_NAME;
 const db_port = process.env.DB_PORT;
 
-const mongoconnecturl = util.format('mongodb://%s:%s@%s:%s/%s', db_user, db_password, db_host, db_port, db_name);
+const mongoconnecturl = util.format('mongodb://%s:%s@%s:%s/%s:27017', db_user, db_password, db_host, db_port, db_name);
 
 module.exports = {
     url: mongoconnecturl
